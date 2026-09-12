51da15a81b306f2098ea1af03715834d85fee5b9
Ktoto-nekto1 <xakim202@yandex.ru>
2026-09-12 12:57:06 +0300

    ДЗ1 .py

--- .py
+++ .py
@@ -1,4 +1,4 @@
-/ Задача 1.1
+# Задача 1.1
 print("Введите 1, если будете вводить сначала числа, а затем знак и 2, если число, знак, число: ")
 a = int(input())
 if a == 1:
@@ -17,7 +17,7 @@ if a == 2:
     if c[1] == '-': print(a-b)
     if c[1] == '*': print(a*b)
     if c[1] == '/': print(a/b)
-/ Задача 1.2
+# Задача 1.2
 def fibo(n):
     if n == 1:
         return 1
@@ -31,4 +31,15 @@ for i in range(1, n+1):
     s = fibo(i)
     a.append(s)
 print(a)
-/ Задача 1.3
+# Задача 1.3
+print('Введите число, которое нужно найти в последовательности: ')
+a = int(input())
+x = a
+print('Введите последовательность и end в конце ')
+m = []
+while a != 'end':
+    a = input()
+    m.append(a)
+for i in range(len(m)-1):
+    if m[i] == str(x):
+        print(i+1)
