b66ca244a8a28a199b134f9f80597afca0dd8cdb
Ktoto-nekto1 <xakim202@yandex.ru>
2026-09-21 20:57:37 +0300

    Update secondhw.py

--- secondhw.py
+++ secondhw.py
@@ -1,1 +1,18 @@
-
+f = open('input.txt', 'r')
+c = open('output.txt', 'w')
+a =''
+op = ''
+for i in range(2):
+  if i == 1:
+    op = f.readline()
+  if i == 0:
+    a = f.readline().split()
+if op == '*':
+  c.write(str(int(a[0])*int(a[1])))
+if op == '+':
+  c.write(str(int(a[0])+int(a[1])))
+if op == '/':
+  c.write(str(int(a[0])/int(a[1])))
+if op == '-':
+  c.write(str(int(a[0])-int(a[1])))
+c.close()
